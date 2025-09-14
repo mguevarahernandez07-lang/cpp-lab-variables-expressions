b19a869cc82a26392ca6e47789f196afd92ae63f
mguevarahernandez07-lang <mguevarahernandez07@gmail.com>
2025-09-14 00:03:15 -0700

    Create A1.cpp

---
+++ A1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10, y = 5, z = 2;
+
+    cout << "Expression 1: x + y * z = " << x + y * z << endl;
+    cout << "Expression 2: (x + y) * z = " << (x + y) * z << endl;
+    cout << "Expression 3: x - y - z = " << x - y - z << endl;
+    cout << "Expression 4: x / y * z = " << x / y * z << endl;
+
+    return 0;
+}
